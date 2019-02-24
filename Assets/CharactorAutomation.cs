c9ae90c4903e95b8b64fed7c2089eae0f19f3656
Yan Youmo <yanyoumo@gmail.com>
2019-02-24 23:40:29 +0800

    Inital Commit

---
+++ Assets/CharactorAutomation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+using ObjectFieldAlignment = Sirenix.OdinInspector.ObjectFieldAlignment;
+
+public class CharactorAutomation : SerializedMonoBehaviour
+{
+    public Text charName;
+    public Image charImage;
+
+    public bool switcher = false;
+
+    [System.Serializable]
+    public struct CharData
+    {
+        [FoldoutGroup("Split/$Name", false)]
+        public int Index;
+
+        [FoldoutGroup("Split/$Name", false)]
+        public string Name;
+
+        [FoldoutGroup("Split/$Name", false)]
+        public Sprite CharSprite;
+
+        [FoldoutGroup("Split/$Name", false)]
+        public string DisplayName;
+    }
+
+    public CharData[] chars;
+
+    public Dictionary<string,int> charIndex;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charName.text = chars[0].Name;
+        charImage.sprite = chars[0].CharSprite;
+    }
+
+    [Button("加载人物", ButtonSizes.Gigantic)]
+    public void Convert()
+    {
+        charIndex=new Dictionary<string, int>();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            charIndex.Add(chars[i].Name,i);
+        }
+    }
+
+    public void NextChar(string name="")
+    {
+        var index = charIndex[name];
+        charName.text = chars[index].DisplayName;
+        charImage.sprite = chars[index].CharSprite;
+    }
+}
---
+++ Assets/SubtitleVisualizer.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.UI;
+using System.IO;
+using System.Linq;
+
+[System.Serializable]
+public struct SingleLine{
+    [FoldoutGroup("Split/$Name", false)]
+    public int StartTime { set; get; }
+
+    [FoldoutGroup("Split/$Name", false)]
+    public string Content { set; get; }
+
+    [FoldoutGroup("Split/$Name", false)]
+    public string PlayerName { set; get; }
+}
+
+public class SubtitleVisualizer : MonoBehaviour
+{
+    private int _ptr = 0;
+
+    private bool _play = false;
+
+    public AudioSource Sound;
+
+    public CharactorAutomation charactorAutomation;
+
+    [Title("字幕效果")]
+
+    [FilePath]
+    public string FilePath = "";
+
+    public Text TargetText;
+    public Text TimerText;
+
+    [ListDrawerSettings(ShowIndexLabels = true, ListElementLabelName = "Content")]
+    public List<SingleLine> Lines;
+
+    [Button("转换", ButtonSizes.Large)]
+    public void Convert() {
+        var reader = new StreamReader(FilePath);
+        Lines = new List<SingleLine>();
+        //reader.ReadLine();
+        while (!reader.EndOfStream)
+        {
+            string str = reader.ReadLine();
+            string timehead= str.Substring(1,10);
+            str = str.Substring(11);
+            string plName= str.Split(']')[0].Substring(1);
+            str =str.Split(']')[1];
+            var line = new SingleLine {Content = str, StartTime = GetTimeStampFromString(timehead), PlayerName=plName };
+            Lines.Add(line);
+        }
+        reader.Close();
+    }
+
+    [Button("播放", ButtonSizes.Large)]
+    public void Play()
+    {
+        _play = true;
+        if (Sound != null)
+        {
+            Sound.time = 0.0f;
+            Sound.Play();
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Convert();
+        _ptr = 0;
+        TargetText.text = Lines[0].Content;
+    }
+
+    int GetTimeStampFromString(string str)
+    {
+        return int.Parse(str.Substring(0,2))*60000+ int.Parse(str.Substring(3, 2)) * 1000 + int.Parse(str.Substring(6, 3));
+    }
+
+    string AddReturn(string str)
+    {
+        string res = "";
+        while (str.Length>20)
+        {
+            res += str.Substring(0, 20) + "\n";
+            str = str.Substring(20);
+        }
+        res += str;
+        return res;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(!_play)return;
+        if (Sound.time*1000.0 >= Lines[_ptr+1].StartTime)
+        {
+            _ptr++;
+            TargetText.text = AddReturn(Lines[_ptr].Content);
+            charactorAutomation.NextChar(Lines[_ptr].PlayerName);
+        }
+    }
+}
